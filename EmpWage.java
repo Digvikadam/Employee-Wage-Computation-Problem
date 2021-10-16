21bfb3bf94a8372952e6f125b1d4e1f69deb3ef7
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-16 22:36:30 +0530

    Added: Calculate-Daily-Wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,11 +3,13 @@ package com.bridgelabz;
 import java.util.Random;
 public class EmpWage {
 	public static void main(String[] args) {
-		System.out.println("Employee Status");
 		Random rd = new Random();
 		int empcheck = rd.nextInt(2);
+		int WAGE_PER_HR = 20;
+		int WORK_HRS = 8;
 		if(empcheck==1) {
 			System.out.println("Employee is Present");
+			System.out.println(WAGE_PER_HR * WORK_HRS);
 		}
 		else {
 			System.out.println("Employee is Absent");
