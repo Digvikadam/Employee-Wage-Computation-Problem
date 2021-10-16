6917df3f333d0a32f866b3b672a9087eea1ce4c4
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-16 23:42:56 +0530

    Added: Calculating-Wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,28 +2,47 @@ package com.bridgelabz;
 
 import java.util.Random;
 public class EmpWage {
-	public static final int TOTAL_WORKING_DAYS = 20;
-	public static final int WAGE_PER_HR = 20;
-	public static final int FULL_TIME_WORK_HRS = 8;
-	public static final int PART_TIME_WORK_HRS = 4;
+	public static final int IS_PRESENT=1;
+	public static final int IS_ABSENT=0;
+	public static final int IS_FULLTIME=8;
+	public static final int IS_PART_TIME=4;
+	public static final int WAGE_PER_HOUR=20;
+	public static final int NUMBER_OF_WORKING_DAYS=20;
+	public static int DAILY_WAGE=0;
+	public static int MONTHLY_WAGE=0;
+	public static int TOTAL_HOURS=0;
+	public static int HOURS=0;
 	public static void main(String[] args) {
-		Random rd = new Random();
-		int empcheck = rd.nextInt(3);
-		switch (empcheck) {
-		case 1:
-			System.out.println("Month Salary of FullTime Employee");
-			System.out.println((FULL_TIME_WORK_HRS  *  WAGE_PER_HR) * TOTAL_WORKING_DAYS  );
-			break;
-		case 2:
-			System.out.println("Month Salary of PartTime Employee");
-			System.out.println( (PART_TIME_WORK_HRS  *  WAGE_PER_HR) * TOTAL_WORKING_DAYS  );
-			break;
-		default:
-			System.out.println("Salary=0");
-		
-		}
+		System.out.println("Welcome to Employee Wage Computation program");
+		for( int i=0; i<NUMBER_OF_WORKING_DAYS && TOTAL_HOURS<100; i++) {
+			Random random = new Random();
+			int Emp_check = random.nextInt(3);
+			switch (Emp_check) {
+				case IS_PRESENT: {
+					System.out.println("Employee is present");
+					DAILY_WAGE = IS_FULLTIME * WAGE_PER_HOUR; HOURS=IS_FULLTIME;
+					break;
+				} 
+				case IS_ABSENT: {
+					System.out.println("Employee is Absent");
+					break;
+				} 
+				default: {
+					System.out.println("Employee is part time ");
+					DAILY_WAGE = IS_PART_TIME * WAGE_PER_HOUR;
+					HOURS=IS_PART_TIME;
+					break;
+				} 
+	       		} 
+			MONTHLY_WAGE = DAILY_WAGE + MONTHLY_WAGE;
+			TOTAL_HOURS = HOURS + TOTAL_HOURS;
+		}	
+	    	System.out.println("Daily wage : " +DAILY_WAGE);
+		System.out.println("Monthly wage : " +MONTHLY_WAGE);
 	}
 }
+
+
 
 
 
