973fa1bc2bd00192f1f56a8bc4dc4db7119efac8
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-16 23:17:42 +0530

    Added: Using-Switch-Case

--- EmpWage.java
+++ EmpWage.java
@@ -2,23 +2,26 @@ package com.bridgelabz;
 
 import java.util.Random;
 public class EmpWage {
+	public static final int WAGE_PER_HR = 20;
+	public static final int FULL_TIME_WORK_HRS = 8;
+	public static final int PART_TIME_WORK_HRS = 4;
 	public static void main(String[] args) {
 		Random rd = new Random();
 		int empcheck = rd.nextInt(3);
-		int WAGE_PER_HR = 20;
-		int FULL_TIME_WORK_HRS = 8;
-		int PART_TIME_WORK_HRS= 4;
-		if(empcheck==1) {
-			System.out.println("FullTime Wage");
-			System.out.println( WAGE_PER_HR * FULL_TIME_WORK_HRS );
-		}
-		else if(empcheck==2) {
-			System.out.println("PartTime Wage");
-			System.out.println( WAGE_PER_HR * PART_TIME_WORK_HRS);
-		}
-		else {
+		
+		switch(empcheck) {
+		case 1:
+			System.out.println("Salary of Fulltime Employee");
+			System.out.println( WAGE_PER_HR * FULL_TIME_WORK_HRS);
+		break;
+		case 2:
+			System.out.println("Salary of Parttime Employee");
+			System.out.println( WAGE_PER_HR * PART_TIME_WORK_HRS );
+		break;
+		default:
 			System.out.println("Salary = 0");
 		}
+		
 	}
 }
 
