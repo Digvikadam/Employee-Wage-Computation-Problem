386321a3ee871539f50d234c171c3fd5a1d30741
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-16 23:09:18 +0530

    Added: Part-Time

--- EmpWage.java
+++ EmpWage.java
@@ -4,15 +4,20 @@ import java.util.Random;
 public class EmpWage {
 	public static void main(String[] args) {
 		Random rd = new Random();
-		int empcheck = rd.nextInt(2);
+		int empcheck = rd.nextInt(3);
 		int WAGE_PER_HR = 20;
-		int WORK_HRS = 8;
+		int FULL_TIME_WORK_HRS = 8;
+		int PART_TIME_WORK_HRS= 4;
 		if(empcheck==1) {
-			System.out.println("Employee is Present");
-			System.out.println(WAGE_PER_HR * WORK_HRS);
+			System.out.println("FullTime Wage");
+			System.out.println( WAGE_PER_HR * FULL_TIME_WORK_HRS );
+		}
+		else if(empcheck==2) {
+			System.out.println("PartTime Wage");
+			System.out.println( WAGE_PER_HR * PART_TIME_WORK_HRS);
 		}
 		else {
-			System.out.println("Employee is Absent");
+			System.out.println("Salary = 0");
 		}
 	}
 }
